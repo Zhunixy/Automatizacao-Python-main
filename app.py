3fd9cc0566c625e5fbc5e2529ed6c9da4489eb0c
Razawaky <viniciusduarte564@gmail.com>
2024-11-07 08:19:48 -0300

    Finalizado

--- app.py
+++ app.py
@@ -18,25 +18,25 @@ def dados():
     # Iterar sobre cada linha do arquivo Excel, começando da segunda linha (índice 2)
     for linha in vendas_sheet.iter_rows(min_row=2):
         # Nome do cliente
-        pyautogui.click(1164,314,duration=0.5)  # Clicar no campo de nome do cliente
+        pyautogui.click(844,311,duration=0.5)  # Clicar no campo de nome do cliente
         pyautogui.write(linha[0].value)  # Digitar o nome do cliente obtido da célula A
         
         # Produto
-        pyautogui.click(1148,406,duration=0.5)  # Clicar no campo de produto
+        pyautogui.click(797,410,duration=0.5)  # Clicar no campo de produto
         pyautogui.write(linha[1].value)  # Digitar o nome do produto obtido da célula B
         
         # Quantidade
-        pyautogui.click(1158,509,duration=0.5)  # Clicar no campo de quantidade
+        pyautogui.click(773,511,duration=0.5)  # Clicar no campo de quantidade
         pyautogui.write(str(linha[2].value))  # Digitar a quantidade como string obtida da célula C
         
         # Categoria do produto
-        pyautogui.click(1353,604,duration=0.5)  # Abrir o menu de seleção de categoria
+        pyautogui.click(789,611,duration=0.5)  # Abrir o menu de seleção de categoria
         pyautogui.write(linha[3].value)  # Digitar a primeira letra da categoria obtida da célula D
-        pyautogui.click(1120,612,duration=0.5)  # Clicar no ícone de seta para baixo
-        pyautogui.click(1187,685,duration=0.5)  # Selecioinar a categoria do produto
-        pyautogui.click(1371,189,duration=0.5)
+        pyautogui.click(955,606,duration=0.5)  # Clicar no ícone de seta para baixo
+        pyautogui.click(813,683,duration=0.5)  # Selecioinar a categoria do produto
+        pyautogui.click(968,192,duration=0.5)
     
                 
-# pyautogui.hotkey('win','6') 
+pyautogui.hotkey('win','4') 
 threading.Thread(target=dados).start()
 threading.Thread(target=stop).start()
